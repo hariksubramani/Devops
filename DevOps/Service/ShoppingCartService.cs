3d57a8b88da79614e3fa66d4e66fe17dde1eb03a
TCIG-Hari <hari@mailcohen.com>
2019-05-18 03:57:45 +0300

    pushing to the branch

---
+++ DevOps/Service/ShoppingCartService.cs
@@ -0,0 +1,31 @@
+﻿using DevOps.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevOps.Service
+{
+    public class ShoppingCartService : IShoppingCartService
+    {
+        public ShoppingItem Add(ShoppingItem newItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ShoppingItem> GetAllItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ShoppingItem GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
